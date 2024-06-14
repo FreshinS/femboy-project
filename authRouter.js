f3a8e247109f9180e799a6fa8dbe68cb45d2d4df
FreshinS <freshinsboost@gmail.com>
2024-06-15 00:51:48 +0300

    YA SDELAL ETU GREBANUYU REGISTRTIU

--- authRouter.js
+++ authRouter.js
@@ -6,8 +6,16 @@ const authMiddleware = require('./middleware/authMiddleware')
 const roleMiddleware = require('./middleware/roleMiddleware')
 
 router.post('/registration', [
-    check('username', "Имя пользователя не может быть пустым").notEmpty(),
-    check('password', "Пароль не может быть короче 4 или длинее 16 символов").isLength({min:4, max:16})
+    check('name', "Имя не может быть пустым").notEmpty(),
+    check('surname', "Фамилия не может быть пустой").notEmpty(),
+    check('email', "Неверный формат почты").isEmail(),
+    check('avatar', "Вставьте автар").notEmpty(),
+    check('birthday', "Введите дату рождения").notEmpty(),
+    check('about', "Заполните поле о себе").notEmpty(),
+    check('password', "Пароль не может быть короче 4 или длинее 16 символов").isLength({min:4, max:16}),
+    check('city', 'Поле "Ваш город/область" не может быть пустым').notEmpty(),
+    check('characters', "Укажите ваших любимых персонажей").notEmpty(),
+    check('anime', "Укажите ваши любимые аниме").notEmpty(),
 ], controller.registration)
 router.post('/login', controller.login)
 router.get('/users', roleMiddleware(['ADMIN']), controller.getUsers)
--- index.js
+++ index.js
@@ -1,13 +1,37 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const cors = require('cors');
+const multer = require('multer');
 const authRouter = require('./authRouter')
-const PORT = process.env.PORT || 5150
+const PORT = process.env.PORT || 5500
 const { serverPassword } = require("./config")
+const path = require('path');
 
 const app = express()
 
+app.use(cors());
 app.use(express.json())
 app.use("/auth", authRouter)
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'uploads/');
+    },
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + path.extname(file.originalname)); // Добавляем дату и расширение файла
+    }
+});
+
+const upload = multer({ storage: storage });
+
+app.post('/upload', upload.single('avatar'), (req, res) => {
+    if (!req.file) {
+        return res.status(400).send({ message: 'No file uploaded' });
+    }
+
+    // Возвращаем URL загруженного файла
+    res.json({ url: `http://localhost:5500/uploads/${req.file.filename}` });
+});
 
 const start = async () => {
     try {
